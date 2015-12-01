be5040402b471b4f71d2ab4fe9eb8ff8fb1ab60c
Felipe Vasconcelos <jikashow@gmail.com>
2015-11-30 22:13:21 -0300

    Adding more stuff

---
+++ src/js/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var app = {
+
+	init: function(){
+		new WOW().init();
+	}
+};
+
+// scroll to top
+$(window).scroll(function() {
+	if ($(this).scrollTop() > 100) {
+		$('#scroll-to-top').fadeIn();
+	} else {
+		$('#scroll-to-top').fadeOut();
+	}
+});
+
+$('#scroll-to-top').on('click', function(event) {
+	event.preventDefault();
+	$('body,html').animate({
+		scrollTop: 0
+	}, 800);
+});
