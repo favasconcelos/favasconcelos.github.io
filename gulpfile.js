00507300d5ee9df7c0e4dcbb6e55bc326a1c6b5a
Felipe Vasconcelos <fav@cesar.org.br>
2015-12-14 15:46:40 -0300

    Adding new layout

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
-var gulp = require('gulp');
+/*var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 
 gulp.task('webserver', function() {
+
 	gulp.src('.').pipe(webserver({
 		fallback: 'index.html',
 		open: true,
@@ -16,6 +17,55 @@ gulp.task('webserver', function() {
 			}
 		}
 	}));
+
 });
 
-gulp.task('default', ['webserver']);
+gulp.task('default', ['webserver']);*/
+
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var sass = require('gulp-sass');
+var webserver = require('gulp-webserver');
+var opn = require('opn');
+
+var sourcePaths = {
+	styles: ['scss/**/*.scss']
+};
+
+var distPaths = {
+	styles: 'css'
+};
+
+var server = {
+	host: 'localhost',
+	port: '8080'
+};
+
+gulp.task('sass', function() {
+	gulp.src(sourcePaths.styles)
+		.pipe(plumber())
+		.pipe(sass())
+		.pipe(gulp.dest(distPaths.styles));
+});
+
+gulp.task('webserver', function() {
+	gulp.src('.')
+		.pipe(webserver({
+			host: server.host,
+			port: server.port,
+			livereload: true,
+			directoryListing: false
+		}));
+});
+
+gulp.task('openbrowser', function() {
+	opn('http://' + server.host + ':' + server.port);
+});
+
+gulp.task('watch', function() {
+	gulp.watch(sourcePaths.styles, ['sass']);
+});
+
+gulp.task('build', ['sass']);
+
+gulp.task('default', ['build', 'webserver', 'watch', 'openbrowser']);
--- js/app.js
+++ js/app.js
@@ -5,25 +5,6 @@ var app = {
 	init: function() {
 		new WOW().init();
 		$('[data-toggle="tooltip"]').tooltip()
-		this.scroll();
-	},
-
-	scroll: function() {
-		// scroll to top
-		$(window).scroll(function() {
-			if ($(this).scrollTop() > 100) {
-				$('#scroll-to-top').fadeIn();
-			} else {
-				$('#scroll-to-top').fadeOut();
-			}
-		});
-
-		$('#scroll-to-top').on('click', function(event) {
-			event.preventDefault();
-			$('body,html').animate({
-				scrollTop: 0
-			}, 800);
-		});
 	}
 
 };
