742ed1ef90e4f82be7ab3a6f4f0045dcfc676386
Felipe Vasconcelos <f@avasconcelos.com>
2019-08-03 23:58:51 +0200

    Updates

---
+++ precache-manifest.a04dbd827ba3806dc8a8cbe104e313ec.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b13e19777432cff73afcd57150c5bef9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe90e9a509f748316d02",
+    "url": "/static/css/main.aeb5cfa4.chunk.css"
+  },
+  {
+    "revision": "856ce651700b3311f177",
+    "url": "/static/js/2.527bc477.chunk.js"
+  },
+  {
+    "revision": "fe90e9a509f748316d02",
+    "url": "/static/js/main.40b62220.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "25b6e0fd67130e1becc31317298e5b02",
+    "url": "/static/media/01.25b6e0fd.jpg"
+  },
+  {
+    "revision": "bb2d17399b8c4cdb5f174e8df7f251e5",
+    "url": "/static/media/02.bb2d1739.jpg"
+  },
+  {
+    "revision": "d14b244615deac10deeca4ad1551902d",
+    "url": "/static/media/03.d14b2446.jpg"
+  },
+  {
+    "revision": "879cfbea090fda322a5a1b42c53350c4",
+    "url": "/static/media/flag_brazil.879cfbea.svg"
+  },
+  {
+    "revision": "9012d90eb92a3b1413d0563c5770c30a",
+    "url": "/static/media/flag_spain.9012d90e.svg"
+  }
+]);
